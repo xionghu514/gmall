9d4de4e1bf50a29385623c6684f2160ca9ba65d5
Guan FuQing <moumouguan@gmail.com>
2022-12-13 20:22:45 +0800

    Add elasticsearch: 2. 封装搜索页所需的数据模型 以及 基于注解生成索引库及映射

--- gmall-search/src/test/java/com/atguigu/gmall/search/GmallSearchApplicationTests.java
+++ gmall-search/src/test/java/com/atguigu/gmall/search/GmallSearchApplicationTests.java
@@ -1,13 +1,26 @@
 package com.atguigu.gmall.search;
 
+import com.atguigu.gmall.search.pojo.Goods;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.IndexOperations;
 
 @SpringBootTest
 class GmallSearchApplicationTests {
 
+    @Autowired
+    private ElasticsearchRestTemplate restTemplate;
+
     @Test
     void contextLoads() {
+        // 索引库的操作对象
+        IndexOperations indexOps = restTemplate.indexOps(Goods.class);
+        // 创建索引库
+        indexOps.create();
+        // 声明映射
+        indexOps.putMapping(indexOps.createMapping());
     }
 
 }
