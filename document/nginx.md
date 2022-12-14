32a1828ddfad5fe75713f88d639ebde763d78644
Guan FuQing <moumouguan@gmail.com>
2022-12-14 14:50:58 +0800

    Add Search: 2. 前后连调 5. 完成搜索功能前后连调

--- document/nginx.md
+++ document/nginx.md
@@ -65,3 +65,17 @@ cd /usr/local/nginx/sbin
 > 浏览器输入域名 -> 本机hosts 文件对此域名进行解析 -> 真实发送请求是 ip + 请求路径 -> 根据 ip + 80 端口找到 nginx 服务器 -> nginx 配置了反向代理根据 域名头信息找到 当前 server -> 根据反向代理后的  ip 地址找到网关应用 -> 网关根据 断言配置 路由到对应的 微服务 -> 找到对应的 controller 方法处理 -> 处理完后沿路返回到用户浏览器
 
 ![](https://oss.yiki.tech/gmall/202211200219709.png)
+
+## 静态资源部署
+
+```
+    # 动静分离
+    server {
+        listen          80; # 监听 80 端口
+        server_name     static.x x x.com; # 服务器地址或绑定域名
+
+        location / { # 访问80端口后的所有路径都转发到 proxy_pass 配置的ip中
+                root /opt/static;
+        }
+    }
+```
--- gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
+++ gmall-search/src/main/java/com/atguigu/gmall/search/controller/SearchController.java
@@ -1,12 +1,12 @@
 package com.atguigu.gmall.search.controller;
 
-import com.atguigu.gmall.common.bean.ResponseVo;
 import com.atguigu.gmall.search.pojo.SearchParamVo;
 import com.atguigu.gmall.search.pojo.SearchResponseVo;
 import com.atguigu.gmall.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Description:
@@ -14,16 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2022/12/14 14:05
  * @Email: moumouguan@gmail.com
  */
-@RestController
+//@RestController
+@Controller
 public class SearchController {
 
     @Autowired
     private SearchService searchService;
 
     @GetMapping("/search")
-    public ResponseVo<SearchResponseVo> search(SearchParamVo paramVo) {
+    public String search(SearchParamVo paramVo, Model model) {
         SearchResponseVo responseVo = searchService.search(paramVo);
 
-        return ResponseVo.ok(responseVo);
+        model.addAttribute("response", responseVo);
+        model.addAttribute("searchParam", paramVo);
+        return "search";
     }
 }
