6d91cabc734963e4c2b452bce67510622a967dd4
Guan FuQing <moumouguan@gmail.com>
2022-12-23 22:16:58 +0800

    Add item: 前后连调

--- gmall-item/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
+++ gmall-item/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
@@ -1,6 +1,5 @@
 package com.atguigu.gmall.item.controller;
 
-import com.atguigu.gmall.common.bean.ResponseVo;
 import com.atguigu.gmall.item.pojo.ItemVo;
 import com.atguigu.gmall.item.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description:
@@ -23,11 +21,12 @@ public class ItemController {
     private ItemService itemService;
 
     @GetMapping("{skuId}.html")
-    @ResponseBody
-    public ResponseVo<ItemVo> loadData(@PathVariable("skuId") Long skuId) {
+//    @ResponseBody
+    public String loadData(@PathVariable("skuId") Long skuId, Model model) {
         ItemVo itemVo = itemService.loadData(skuId);
+        model.addAttribute("itemVo", itemVo);
 
-        return ResponseVo.ok(itemVo);
+        return "item";
     }
 
 }
