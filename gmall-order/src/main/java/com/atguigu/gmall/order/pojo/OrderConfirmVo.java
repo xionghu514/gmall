7aeffc52854c0df4ef7a83fa79635d056d46759b
Guan FuQing <moumouguan@gmail.com>
2022-12-27 02:41:37 +0800

    Add order: 3. 订单确认页数据模型的设计及防重复提交

---
+++ gmall-order/src/main/java/com/atguigu/gmall/order/pojo/OrderConfirmVo.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.order.pojo;
+
+import com.atguigu.gmall.ums.entity.UserAddressEntity;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Guan FuQing
+ * @Date: 2022/12/27 02:17
+ * @Email: moumouguan@gmail.com
+ */
+@Data
+public class OrderConfirmVo {
+
+    // 收货地址列表
+    private List<UserAddressEntity> addresses;
+
+    // 送货清单, 根据购物车页面传递过来的 skuIds 查询
+    private List<OrderItemVo> items;
+
+    // 用户的购物积分信息，ums_member表中的integration字段
+    private Integer bounds;
+
+    // 防重的唯一标识
+    private String orderToken;
+
+}
---
+++ gmall-order/src/main/java/com/atguigu/gmall/order/pojo/OrderItemVo.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.order.pojo;
+
+import com.atguigu.gmall.pms.entity.SkuAttrValueEntity;
+import com.atguigu.gmall.sms.vo.ItemSaleVo;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Guan FuQing
+ * @Date: 2022/12/27 02:20
+ * @Email: moumouguan@gmail.com
+ */
+@Data
+public class OrderItemVo {
+
+    private Long skuId;
+    private String title;
+    private String defaultImage;
+    private BigDecimal price;
+    private Integer count;
+    private BigDecimal weight;
+    private List<SkuAttrValueEntity> saleAttrs; // 销售属性
+    private List<ItemSaleVo> sales; // 营销信息
+    private Boolean store = false; // 库存信息
+
+}
