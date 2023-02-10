fb56ec663417af8767bc949fd3b41c8dd1c5185d
xionghu <1796235969@qq.com>
2023-02-10 16:10:24 +0800

    Add BaseCRUD: 根据分页参数 和 分类id 和 关键字 查询spu

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SpuController.java
@@ -1,23 +1,21 @@
 package com.atguigu.gmall.pms.controller;
 
-import java.util.List;
-
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.ResponseVo;
+import com.atguigu.gmall.pms.entity.SpuEntity;
+import com.atguigu.gmall.pms.service.SpuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.atguigu.gmall.pms.entity.SpuEntity;
-import com.atguigu.gmall.pms.service.SpuService;
-import com.atguigu.gmall.common.bean.PageResultVo;
-import com.atguigu.gmall.common.bean.ResponseVo;
-import com.atguigu.gmall.common.bean.PageParamVo;
+import java.util.List;
 
 /**
  * spu信息
@@ -33,6 +31,25 @@ public class SpuController {
 
     @Autowired
     private SpuService spuService;
+
+
+
+    /**
+     * 请求路径：http://api.gmall.com/pms/spu/category/{cid}?pageNum=1&pageSize=10&key=
+     *
+     * @param cid
+     * @param paramVo
+     * @return
+     */
+    @GetMapping("category/{cid}")
+    public ResponseVo<PageResultVo> querySpusByCidAndPageNumAndPageSize(
+            @PathVariable("cid") Long cid,
+            PageParamVo paramVo
+    ) {
+        PageResultVo pageResultVo = spuService.querySpusByCidAndPageNumAndPageSize(cid, paramVo);
+
+        return ResponseVo.ok(pageResultVo);
+    }
 
     /**
      * 列表
--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
@@ -1,16 +1,16 @@
 package com.atguigu.gmall.pms.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.pms.entity.SpuEntity;
+import com.atguigu.gmall.pms.mapper.SpuMapper;
+import com.atguigu.gmall.pms.service.SpuService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.atguigu.gmall.common.bean.PageResultVo;
-import com.atguigu.gmall.common.bean.PageParamVo;
-
-import com.atguigu.gmall.pms.mapper.SpuMapper;
-import com.atguigu.gmall.pms.entity.SpuEntity;
-import com.atguigu.gmall.pms.service.SpuService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
 
 
 @Service("spuService")
@@ -21,6 +21,30 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements
         IPage<SpuEntity> page = this.page(
                 paramVo.getPage(),
                 new QueryWrapper<SpuEntity>()
+        );
+
+        return new PageResultVo(page);
+    }
+
+    @Override
+    public PageResultVo querySpusByCidAndPageNumAndPageSize(Long cid, PageParamVo paramVo) {
+        LambdaQueryWrapper<SpuEntity> wrapper = new LambdaQueryWrapper<>();
+
+        if (cid != 0) {
+            wrapper.eq(SpuEntity::getCategoryId, cid);
+        }
+
+        // 获取关键字
+        String key = paramVo.getKey();
+
+        // 判空
+        if (StringUtils.isNotBlank(key)) {
+            wrapper.eq(SpuEntity::getId, key).or().like(SpuEntity::getName, key);
+        }
+
+        IPage<SpuEntity> page = this.page(
+                paramVo.getPage(),
+                wrapper
         );
 
         return new PageResultVo(page);
